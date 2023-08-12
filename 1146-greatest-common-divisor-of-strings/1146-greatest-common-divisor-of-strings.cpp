559f086ebc11424ef939a3fc7c3a6871a5188942
Mrunmayee-762004 <109156260+Mrunmayee-762004@users.noreply.github.com>
2023-08-13 01:31:47 +0530

    Time: 5 ms (38.27%), Space: 7.2 MB (57.33%) - LeetHub

---
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        // int lgt1 = str1.size();
+        // int lgt2 = str2.size();
+        // int c=0;
+        // for(int i=1; i<lgt1/2; i++){
+        //     if(str1.substr(i, lgt2) == str2){
+        //         c++;
+        //     }
+        // }
+        // if(c==0){
+        //     return "";
+        // }
+        // else{
+        //     // if(str2)
+        //     return str2;
+        // }
+//     int gcd(int a, int b) {
+//     while (b) {
+//         a = a % b;
+//         std::swap(a, b);
+//     }
+//     return a;
+// }
+    if (str1 + str2 != str2 + str1) {
+        return "";
+    }
+
+    return str1.substr(0, gcd(str1.length(), str2.length()));
+}
+};
